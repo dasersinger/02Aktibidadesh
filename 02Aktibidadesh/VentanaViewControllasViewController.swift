dfba7d051485afbcf3b0b14574ad0f17bacf3a2e
faith1g <dasersinger@gmail.com>
2023-10-05 10:06:41 +0200

    EjemploFinalizado

--- 02Aktibidadesh/VentanaViewControllasViewController.swift
+++ 02Aktibidadesh/VentanaViewControllasViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  VentanaViewControllasViewController.swift
 //  02Aktibidadesh
 //
 //  Created by Mihai Anghel on 05/10/2023.
@@ -7,31 +7,30 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class VentanaViewControllasViewController: UIViewController {
     //EQUIVALENTE A ONCREATE
     override func viewDidLoad() {
         super.viewDidLoad()
-        print("Estoy en el metodo \(#function) de VENTANA 1")
+        print("Estoy en el metodo \(#function) de VENTANA 2")
     }
     
     //EQUIVALE A ONSTART
     override func viewWillAppear(_ animated: Bool) {
-        print("Estoy en el metodo \(#function) de VENTANA 1")
+        print("Estoy en el metodo \(#function) de VENTANA 2")
     }
     
     //EQUIVALE A ONRESUME
     override func viewDidLayoutSubviews() {
-        print("Estoy en el metodo \(#function) de VENTANA 1")
+        print("Estoy en el metodo \(#function) de VENTANA 2")
     }
     
     //EQUIVALE A ONSTOP DE ANDROID STUDIO
     override func viewWillDisappear(_ animated: Bool) {
-        print("Estoy en el metodo \(#function) de VENTANA 1")
+        print("Estoy en el metodo \(#function) de VENTANA 2")
     }
     
     //EQUIVALE A ONDESTROY
     override func viewDidDisappear(_ animated: Bool) {
-        print("Estoy en el metodo \(#function) de VENTANA 1")
+        print("Estoy en el metodo \(#function) de VENTANA 2")
     }
 }
-
