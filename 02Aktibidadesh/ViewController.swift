47328e6da9e8f144407c76e938048aacb0cb823e
faith1g <dasersinger@gmail.com>
2023-10-05 09:23:25 +0200

    Initial Commit

---
+++ 02Aktibidadesh/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  02Aktibidadesh
+//
+//  Created by Mihai Anghel on 05/10/2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
